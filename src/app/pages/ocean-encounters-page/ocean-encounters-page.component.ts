adede82bffd5d4d0a7addb0dba276feac16cda44
Sean Alexander <invadersean@gmail.com>
2022-10-22 09:21:07 -0700

    Adds typing to the Encounters array

--- src/app/pages/ocean-encounters-page/ocean-encounters-page.component.ts
+++ src/app/pages/ocean-encounters-page/ocean-encounters-page.component.ts
@@ -10,7 +10,6 @@ export class OceanEncountersPageComponent implements OnInit {
 
   numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  // TODO: Type Encounters.
   encounters: Encounter[] = [];
 
   constructor() { }
